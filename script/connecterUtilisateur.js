2355b5f33d48e126115cb3f0a8908b62a824b340
Sazerat <namulabre@gmail.com>
2022-03-11 15:17:09 +0100

    Ajout de fichier pour le style des vues

--- script/connecterUtilisateur.js
+++ script/connecterUtilisateur.js
@@ -1,19 +1,22 @@
 $.fn.connecterUtilisateur = function ({ container, user, success, error }) {
-    $(container + " input[type=button]").on("click", login);
+  $(container + " input[type=button]").on("click", login);
 
-    function login() {
-      let isLogin = false;
-      //1. chopper info user
-      //2. Verifier info dans "BDD"
-      //3. Si existe => on redirige (http.get(mapage)) Sinon => Msg d'erreur
-      let nom = $(container + ">#nom");
-      let code = $(container + ">#code");
-      //On va chercher l'utilisateur dans l'API
-      let data = $.getJSON('http://vps.e-mingo.net/coopagri/app/index.php?c=api&x=1&n=CompteIndividu&where=login|' + nom, (json) => {
-        console.log(json)
-      });
-      console.log(data);
-      return isLogin;
-    }
+  function login() {
+    //1. chopper info user
+    //2. Verifier info dans "BDD"
+    //3. Si existe => on redirige (http.get(mapage)) Sinon => Msg d'erreur
+    let nom = $(container + ">#nom");
+    let code = $(container + ">#code");
+    //On va chercher l'utilisateur dans l'API
+    let url = `http://vps.e-mingo.net/coopagri/app/index.php?c=api&x=1&n=CompteIndividu&where=login|${nom}&where=pass|${code}`;
 
+    let data = $.ajax({
+      dataType: "jsonp",
+      url: url,
+      success: function (json) {
+        console.debug(json);
+        json.length != 0 ? success() : error();
+      },
+    });
+  }
 };

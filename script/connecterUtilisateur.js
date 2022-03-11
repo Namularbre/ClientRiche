1d9a0e5f9571b1b5918916a69c8946e4e4e1909d
Sazerat <namulabre@gmail.com>
2022-03-11 16:22:57 +0100

    wip

--- script/connecterUtilisateur.js
+++ script/connecterUtilisateur.js
@@ -5,18 +5,25 @@ $.fn.connecterUtilisateur = function ({ container, user, success, error }) {
     //1. chopper info user
     //2. Verifier info dans "BDD"
     //3. Si existe => on redirige (http.get(mapage)) Sinon => Msg d'erreur
-    let nom = $(container + ">#nom");
-    let code = $(container + ">#code");
+    let login = $(container + ">#nom").val();
+    let pass = $(container + ">#code").val();
     //On va chercher l'utilisateur dans l'API
-    let url = `http://vps.e-mingo.net/coopagri/app/index.php?c=api&x=1&n=CompteIndividu&where=login|${nom}&where=pass|${code}`;
-
+    let url = `http://vps.e-mingo.net/coopagri/app/index.php?c=api&o=auth&name=${login}&val=${pass}`
     let data = $.ajax({
       dataType: "jsonp",
       url: url,
-      success: function (json) {
-        console.debug(json);
-        json.length != 0 ? success() : error();
+      crossDomain: true,
+      success: function(json) {
+        console.log("test", json);
+        // json.length != 0 ? success() : error();
       },
+      error: function(t,e, l) {
+          console.log(t,e,l)
+          // error()
+      },
+      complete: function(x) {
+        console.log(x)
+      }
     });
   }
 };

88d5d5b3145ce54451ffc3234ce01ef624ef3f4a
Sazerat <namulabre@gmail.com>
2022-03-11 14:23:23 +0100

    Changement du formulaire de connection

--- script/connecterUtilisateur.js
+++ script/connecterUtilisateur.js
@@ -1,16 +1,25 @@
-(function($){
-    $fn.connecterUtilisateur = function ({container,user,success,error}) {
-        //1. chopper info user
-        //2. Verifier info dans "BDD"
-        //3. Si existe => on redirige (http.get(mapage)) Sinon => Msg d'erreur
-        let nom = $(this + ">#nom");
-        let prenom = $(this + ">#prenom");
-        let code = $(this + ">#code");
+(function ($) {
+  $fn.connecterUtilisateur = function ({ container, user, success, error }) {
+    $(container + "input[type=submit]").on("click", login);
 
-        //chercher dans données
-        if (islogin) {
-            return success;
-        }
-        return error;
+    function login() {
+      //1. chopper info user
+      //2. Verifier info dans "BDD"
+      //3. Si existe => on redirige (http.get(mapage)) Sinon => Msg d'erreur
+      let nom = $(container + ">#nom");
+      let code = $(container + ">#code");
+
+      let test = $.JSON(
+        "http://vps.e-mingo.net/coopagri/app/index.php?c=api&n=CompteIndividu&a=get&x=1"
+      );
+      console.log(test);
+      //chercher dans données
+      return isLogin;
     }
-})
+
+
+
+
+
+  };
+});

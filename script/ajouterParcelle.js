646739be37611a189c7b269ef257826bce907859
Sazerat <namulabre@gmail.com>
2022-03-17 22:03:53 +0100

    wip

--- script/ajouterParcelle.js
+++ script/ajouterParcelle.js
@@ -1,3 +1,47 @@
 $.fn.ajouterParcelle = function ({container, user , success, error}) {
+
+    function ajouterAdresseAPI(){
+        let rueNumero = $("#Numero").val()
+        let nomRue = $("#Adresse").val()
+        let codePostal = $("#Codepostal").val()
+        let ville = $("#Ville").val()
+     
+         // ajouter l'adresse
+         let url = `http://vps.e-mingo.net/coopagri/app/index.php?c=Adresse&a=enregistrer&entId=${user.id}&societeId=${user.societe.id}&rueNumero=${rueNumero}&rueNom=${nomRue}&codePostal=${codePostal}&ville=${ville}`
+     
+         $.ajax({
+             url,
+             crossDomain: true,
+             success: function(json) {
+               ajouterParcelleAPI(json.id)     
+             },
+             error: function(t,e, l) {
+               error();
+             },
+         })
+    }
+
+    function ajouterParcelleAPI(idAdresse){
+         // ajouter la parcelle
+         let description = $("#Description").val()
+         let libelle = $("#Libelle").val()
+         let url = `http://vps.e-mingo.net/coopagri/app/index.php?c=Parcelle&a=enregistrer&entId=${user.id}&exploitantId=${null}&adresseId=${idAdresse}&libelle=${libelle}&description=${description}`
+     
+         $.ajax({
+             url,
+             crossDomain: true,
+             success: function(json) {
+                success() 
+             },
+             error: function(t,e, l) {
+               error();
+             },
+         })
+    }
+
+
     /*exploitant, adresse {num, addresse, code postal, ville}, libelle, description*/
+    // http://vps.e-mingo.net/coopagri/app/index.php?c=Parcelle&a=enregistrer&entId=0&exploitantId=11&adress=9
+    // http://vps.e-mingo.net/coopagri/app/index.php?c=Adresse&a=enregistrer&entId=0&societeId=11&rueNumero=100&rueNom=test&codePostal=87000&ville=limoges
+    
 }
--- script/getAdresse.js
+++ script/getAdresse.js
@@ -1,8 +1,8 @@
-$.fn.getAdresse = function (geocoder) {
-    //geocoder et map son déclarer en dehors de tout module, dans la vue du menu (/views/menu.html)
-    let resultat = {
-        lat: null,
-        lng: null,
-    };
-    return resultat;
-}
+// $.fn.getAdresse = function (geocoder) {
+//     //geocoder et map son déclarer en dehors de tout module, dans la vue du menu (/views/menu.html)
+//     let resultat = {
+//         lat: null,
+//         lng: null,
+//     };
+//     return resultat;
+// }

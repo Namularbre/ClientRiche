14d25668f947036c6b7dae23df0b37f15b4a0d5f
xernois <antoine2496@gmail.com>
2022-03-18 15:16:51 +0100

    ajout parcelle quasi bon

--- script/ajouterParcelle.js
+++ script/ajouterParcelle.js
@@ -1,47 +1,54 @@
-$.fn.ajouterParcelle = function ({container, user , success, error}) {
+$.fn.ajouterParcelle = function ({ container, user, success, error }) {
+  $($(this)[0]).on("click", "#ajouterParcelle", function () {
+    ajouterAdresseAPI();
+  });
 
-    function ajouterAdresseAPI(){
-        let rueNumero = $("#Numero").val()
-        let nomRue = $("#Adresse").val()
-        let codePostal = $("#Codepostal").val()
-        let ville = $("#Ville").val()
-     
-         // ajouter l'adresse
-         let url = `http://vps.e-mingo.net/coopagri/app/index.php?c=Adresse&a=enregistrer&entId=${user.id}&societeId=${user.societe.id}&rueNumero=${rueNumero}&rueNom=${nomRue}&codePostal=${codePostal}&ville=${ville}`
-     
-         $.ajax({
-             url,
-             crossDomain: true,
-             success: function(json) {
-               ajouterParcelleAPI(json.id)     
-             },
-             error: function(t,e, l) {
-               error();
-             },
-         })
-    }
+  function ajouterAdresseAPI() {
+    let rueNumero = $("#Numero").val();
+    let nomRue = $("#Adresse").val();
+    let rueType = $("#typeRue").val();
+    let codePostal = $("#Codepostal").val();
+    let ville = $("#Ville").val();
 
-    function ajouterParcelleAPI(idAdresse){
-         // ajouter la parcelle
-         let description = $("#Description").val()
-         let libelle = $("#Libelle").val()
-         let url = `http://vps.e-mingo.net/coopagri/app/index.php?c=Parcelle&a=enregistrer&entId=${user.id}&exploitantId=${null}&adresseId=${idAdresse}&libelle=${libelle}&description=${description}`
-     
-         $.ajax({
-             url,
-             crossDomain: true,
-             success: function(json) {
-                success() 
-             },
-             error: function(t,e, l) {
-               error();
-             },
-         })
-    }
+    // encode URI pour remplacer les espaces par des %20 par exemple
+    let url = encodeURI(
+      `http://vps.e-mingo.net/coopagri/app/index.php?c=Adresse&x=1&a=enregistrer&entId=${user.id}&rueNumero=${rueNumero}&rueTypeId=${rueType}&rueNom=${nomRue}&codePostal=${codePostal}&ville=${ville}`
+    );
 
+    $.ajax({
+      url,
+      crossDomain: true,
+      success: function (json) {
+        
+        ajouterParcelleAPI(JSON.parse(json).id);
+      },
+      error: function (t, e, l) {
+        error();
+      },
+    });
+  }
 
-    /*exploitant, adresse {num, addresse, code postal, ville}, libelle, description*/
-    // http://vps.e-mingo.net/coopagri/app/index.php?c=Parcelle&a=enregistrer&entId=0&exploitantId=11&adress=9
-    // http://vps.e-mingo.net/coopagri/app/index.php?c=Adresse&a=enregistrer&entId=0&societeId=11&rueNumero=100&rueNom=test&codePostal=87000&ville=limoges
-    
-}
+  function ajouterParcelleAPI(idAdresse) {
+    // ajouter la parcelle
+    let description = $("#Description").val() || "desc";
+    let libelle = $("#Libelle").val() || "titre";
+
+    let url = encodeURI(`http://vps.e-mingo.net/coopagri/app/index.php?c=Parcelle&x=1&a=enregistrer&entId=${user.id}&exploitantId=${user.societe.id}&adresseId=${idAdresse}&libelle=${libelle}&description=${description}`);
+
+    $.ajax({
+      url,
+      crossDomain: true,
+      success: function (json) {
+        console.log(json)
+        success();
+      },
+      error: function (t, e, l) {
+        error();
+      },
+    });
+  }
+
+  /*exploitant, adresse {num, addresse, code postal, ville}, libelle, description*/
+  // http://vps.e-mingo.net/coopagri/app/index.php?c=Parcelle&a=enregistrer&entId=0&exploitantId=11&adress=9
+  // http://vps.e-mingo.net/coopagri/app/index.php?c=Adresse&a=enregistrer&entId=0&societeId=11&rueNumero=100&rueNom=test&codePostal=87000&ville=limoges
+};
--- script/trouverParcelle.js
+++ script/trouverParcelle.js
@@ -1,7 +1,7 @@
 $.fn.trouverParcelle = function ({container, user, success, error}) {
-    //Pour tester : couz1
+    if(!user?.societe?.parcelles) return error("parcelles indisponibles")
+
     user.societe.parcelles.forEach(parcelle => {
-        //cette fonction passée en paramètre doit creer un <li><h2>adresse de la parcelle</h2><p>produit de la parcelle</p>...
-        success(parcelle);
+        parcelle?.libelle && success(parcelle)
     });
 }

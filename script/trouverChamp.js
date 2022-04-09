82831cfd9c18deadb719272cf4b954a72c84f357
Sazerat <namulabre@gmail.com>
2022-04-09 19:37:47 +0200

    Suppression du nouveau fichier, il est inutile

--- script/trouverChamp.js
+++
@@ -1,9 +0,0 @@
-$.fn.trouverChamp = function ({user, content, sucess, error}) {
-    if (!user?.societe?.parcelle.champs) {
-        return error("Il n'y a pas de champs dans la parcelle");
-    }
-
-    user.societe.parcelles.champs.forEach(champ => {
-        champ?.libelle && sucess(champ);
-    });
-}
